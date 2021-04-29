e62e44971c7afd0b11bab786a2d85d44dc07e835
Rois <rois.khoiron@gmail.com>
2021-04-29 14:12:59 +0700

    mengambil data(fetching) categories dengan axios

--- src/components/ListCategories.js
+++ src/components/ListCategories.js
@@ -1,7 +1,30 @@
 import React, { Component } from "react";
 import { Col } from "react-bootstrap";
+import { API_URL } from "../utils/constants";
+import axios from "axios";
 
 export default class ListCategories extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      categories: [],
+    };
+  }
+
+  componentDidMount() {
+    axios
+      .get(API_URL + "categories")
+      .then((res) => {
+        console.log("Response : ", res);
+        const categories = res.data;
+        this.setState({ categories });
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  }
+
   render() {
     return (
       <Col md={2} mt="2">
