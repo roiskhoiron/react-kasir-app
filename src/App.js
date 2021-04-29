eac0507b3824bb4ea7119d953e65784fefe453bb
Rois <rois.khoiron@gmail.com>
2021-04-29 12:42:18 +0700

    pembuatan navbar dan css font, membuat komponen list dan hasil menggunakan col pemanggilan component dalam index.js mewakili component items

--- src/App.js
+++ src/App.js
@@ -1,10 +1,25 @@
 import "./App.css";
-import NavbarComponent from "./components/NavbarComponent";
+import { Hasil, NavbarComponent, ListCategories } from './components'
+import { Col, Row, Container } from "react-bootstrap";
 
 function App() {
   return (
     <div className="App">
       <NavbarComponent />
+      <div className="mt-3 text-justify">
+        <Container fluid>
+          <Row>
+            <ListCategories />
+            <Col>
+              <h4>
+                <strong>Daftar Produk</strong>
+              </h4>
+              <hr />
+            </Col>
+            <Hasil />
+          </Row>
+        </Container>
+      </div>
     </div>
   );
 }
