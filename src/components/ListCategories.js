aaf7498c2cc966e0796105c3ebd96bc1dbcee267
Rois <rois.khoiron@gmail.com>
2021-04-29 14:31:40 +0700

    menambahkan icon pada listgroup masing" category

--- src/components/ListCategories.js
+++ src/components/ListCategories.js
@@ -2,6 +2,16 @@ import React, { Component } from "react";
 import { Col, ListGroup } from "react-bootstrap";
 import { API_URL } from "../utils/constants";
 import axios from "axios";
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import {faUtensils, faCoffee, faCheese} from '@fortawesome/free-solid-svg-icons'
+
+const Icon = ({ nama }) => {
+  if(nama === "Makanan") return <FontAwesomeIcon icon={faUtensils} className="mr-2"/>;
+  if(nama === "Minuman") return <FontAwesomeIcon icon={faCoffee} className="mr-2" />;
+  if (nama === "Cemilan") return <FontAwesomeIcon icon={faCheese} className="mr-2" />;
+  
+  return <FontAwesomeIcon icon={faUtensils} className="mr-2" />;
+}
 
 export default class ListCategories extends Component {
   constructor(props) {
@@ -37,7 +47,9 @@ export default class ListCategories extends Component {
         <ListGroup>
           {categories &&
             categories.map((category) => (
-              <ListGroup.Item>{category.nama}</ListGroup.Item>
+              <ListGroup.Item key={category.id}>
+                <h5><Icon nama={category.nama} /> {category.nama}</h5>
+              </ListGroup.Item>
             ))}
         </ListGroup>
       </Col>
