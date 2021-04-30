93832b0b356d102ce84cde454e970c91e9e39e58
Rois Khoiron <rois.khoiron@gmail.com>
2021-04-30 14:26:07 +0700

    menampilkan dialog/Modal

--- src/components/Hasil.js
+++ src/components/Hasil.js
@@ -1,9 +1,35 @@
 import React, { Component } from "react";
-import { Badge, Col, ListGroup, Row } from "react-bootstrap";
+import { Button, Badge, Col, ListGroup, Modal, Row } from "react-bootstrap";
 import { numberWithCommas } from "../utils/utils";
 import Purchasing from "./Purchasing";
 
 export default class Hasil extends Component {
+
+  constructor(props) {
+    super(props)
+  
+    this.state = {
+       showDialog: false,
+       keranjangDetail: false,
+       jumlah: 0,
+       keterangan: ''
+    }
+  }
+
+  handleShow = (item) => {
+    this.setState({
+      showDialog: true,
+      keranjangDetail: item,
+    })
+  }
+
+  handleClose = () => {
+    this.setState({
+      showDialog: false
+    })
+  }
+  
+
   render() {
     const { keranjangs } = this.props;
 
@@ -16,7 +42,10 @@ export default class Hasil extends Component {
         {keranjangs !== 0 && (
           <ListGroup variant='flush'>
             {keranjangs.map((item) => (
-              <ListGroup.Item>
+              <ListGroup.Item
+                key={item.id}
+                onClick={() => this.handleShow(item)}
+              >
                 <Row>
                   <Col xs={2}>
                     <h4>
@@ -37,6 +66,23 @@ export default class Hasil extends Component {
                 </Row>
               </ListGroup.Item>
             ))}
+
+            <Modal show={this.state.showDialog} onHide={this.handleClose}>
+              <Modal.Header closeButton>
+                <Modal.Title>Modal heading</Modal.Title>
+              </Modal.Header>
+              <Modal.Body>
+                Woohoo, you're reading this text in a modal!
+              </Modal.Body>
+              <Modal.Footer>
+                <Button variant='secondary' onClick={this.handleClose}>
+                  Close
+                </Button>
+                <Button variant='primary' onClick={this.handleClose}>
+                  Save Changes
+                </Button>
+              </Modal.Footer>
+            </Modal>
           </ListGroup>
         )}
 
